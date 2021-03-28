091998a89e85d416b1f7b3852de68e7d262afd88
Etienne Sibeaux <esibeaux.pro@outlook.fr>
2021-03-28 01:29:55 +0100

    blipblop

---
+++ assets/Tiled/Map_TileSet1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Map_TileSet1" tilewidth="256" tileheight="256" tilecount="256" columns="16">
+ <image source="TILED1.png" width="4096" height="4096"/>
+</tileset>
